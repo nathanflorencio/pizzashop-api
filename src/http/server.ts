6f5e87a90af65ac34d3b29cec449ebfad947c7e1
Nathan Florencio <nflorencio@showcasepro.com.br>
2024-06-30 01:35:25 -0300

    feat: implementing cancel, deliver and dispatch orders routers

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,9 @@ import { getProfile } from './routes/get-profile'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
 import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { dispatchOrder } from './routes/dispatch-order'
+import { deliverOrder } from './routes/deliver-order'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -17,6 +20,9 @@ const app = new Elysia()
   .use(getManagedRestaurant)
   .use(getOrderDetails)
   .use(approveOrder)
+  .use(cancelOrder)
+  .use(dispatchOrder)
+  .use(deliverOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
